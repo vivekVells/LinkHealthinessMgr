70b8868ad756ad0a766e20df5c16cddac2026ba7
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-07-29 00:24:20 -0500

    file upload and link verification successful

--- website/linkHealthMgrPrj/fileuploadviewapp/views.py
+++ website/linkHealthMgrPrj/fileuploadviewapp/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import time, requests
+
 def index(request):
     # return HttpResponse("Hai there! Welcome to Link Healthiness Verifier")
     return render(request, 'fileuploadviewapp/index.html', {})
+
+def upload(request):
+    if request.method == 'POST':
+        noerror = []
+        error404 = []
+
+        file=request.FILES['file'].read().splitlines()
+
+        for f in file:
+            decoded = f.decode("ascii")
+
+            try:
+                r = requests.get((decoded), timeout=5)
+
+                if(int(r.status_code) == 404):
+                    error404.append(decoded)
+                else:
+                    noerror.append(decoded)
+            except requests.exceptions.ConnectionError:
+                print("connection error...")
+
+        context = {'error404' : error404, 'noerror' : noerror }
+        return render(request, 'fileuploadviewapp/index.html', context)
+    else:
+        return HttpResponse("File Upload Error...")
