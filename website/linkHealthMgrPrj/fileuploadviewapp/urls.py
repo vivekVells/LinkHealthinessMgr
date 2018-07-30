7c025d58938c6198cfe55907860e43e0b7e4ae14
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-07-29 19:43:45 -0500

    Switchurlupload - url or file link verify done (#13)
    verify link by url or file upload functionality done. js usage also implemented

--- website/linkHealthMgrPrj/fileuploadviewapp/urls.py
+++ website/linkHealthMgrPrj/fileuploadviewapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload', views.upload, name='upload'),
+    path('verifyByFile', views.verifyByFile, name='verifyByFile'),
+    path('verifyByUrl', views.verifyByUrl, name='verifyByUrl'),
 ]
