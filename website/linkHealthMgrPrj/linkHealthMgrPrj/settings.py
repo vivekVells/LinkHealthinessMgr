0f86b16d42f6f2b56043ad48721d1a232a1f0e4e
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-08-06 23:51:31 -0500

    disabling debug mode

--- website/linkHealthMgrPrj/linkHealthMgrPrj/settings.py
+++ website/linkHealthMgrPrj/linkHealthMgrPrj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(&c%z(897maa51v++um4)dorcg65nh!__sohkeqed)fa#o68u+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
